74f24c4a2f5ad097d267a2e2c3c1e5bbbae775ea
YuraSokolovskyi <sokolovskiiyura@gmail.com>
2023-10-03 14:35:29 -0400

    Client added

---
+++ Client/Client.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Client;
+
+public class Client
+{
+    private TcpClient _client;
+
+    public Client(TcpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task connect(IPEndPoint endPoint)
+    {
+        _client.ConnectAsync(endPoint);
+    }
+
+    public async Task writeToServer(string message)
+    {
+        using (StreamWriter writer = new StreamWriter(_client.GetStream(), leaveOpen:true))
+        {
+            await writer.WriteLineAsync(message);
+        }
+    }
+
+    public async Task<string> readFromServer()
+    {
+        string response = "";
+        using (StreamReader reader = new StreamReader(_client.GetStream(), leaveOpen:true))
+        {
+            response = await reader.ReadLineAsync();
+        }
+
+        return response;
+    }
+
+    public async Task<string> getQuote()
+    {
+        await writeToServer("GET");
+        return await readFromServer();
+    }
+}
